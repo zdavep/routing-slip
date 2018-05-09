4291b0a03ff2b094def3218ae4ce6a2212cdfeac
Dave Pederson <dave.pederson@t12y.com>
2018-05-09 12:09:19 -0600

    Log errors on process failure in Rule

--- src/main/scala/zdavep/Rule.scala
+++ src/main/scala/zdavep/Rule.scala
@@ -1,12 +1,13 @@
 package zdavep
 
-import akka.actor.{ Actor, ActorRef }
+import akka.actor.{ Actor, ActorLogging, ActorRef }
 import scala.concurrent.Future
+import scala.util.{ Failure, Success }
 
 /**
  * Process a message and send the result to the next rule.
  */
-trait Rule[A <: AnyRef] extends Actor with Routing {
+trait Rule[A <: AnyRef] extends Actor with ActorLogging with Routing {
 
   // Execution context
   import context.dispatcher
@@ -20,9 +21,11 @@ trait Rule[A <: AnyRef] extends Actor with Routing {
    * Receive and process a message then route to the next rule.
    */
   def receive: Receive = {
-    case RoutingSlip(rules: Seq[ActorRef], payload: A) ⇒
-      process(payload).map { processed ⇒
-        route(rules, processed)
-      }
+    case RoutingSlip(rules: Seq[ActorRef], payload: A) ⇒ process(payload).onComplete {
+      case Success(processed) ⇒ route(rules, processed)
+      case Failure(t) ⇒
+        log.error("Rule processing failed: {}", t)
+        route(rules, payload)
+    }
   }
 }
